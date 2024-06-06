61000de3fcfd61ee95ae6c23c09269b7298b3f8d
Kashif <haque.kashiful7@gmail.com>
2024-06-06 08:10:07 +0000

    Added hostname

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::fs;
+use std::{fs, os::unix::ffi::OsStringExt};
 use regex::Regex;
 use users::{ get_user_by_uid, get_current_uid };
 
 fn main() {
+  // Get the current OS name
   const OS_DETAILS_FILE_PATH: &str = "/etc/os-release";
   let contents: String = fs::read_to_string(OS_DETAILS_FILE_PATH).expect("not linux");
 
@@ -25,6 +26,23 @@ fn main() {
 
   println!("{}", os_name);
 
+  // Get the current username
   let user = get_user_by_uid(get_current_uid()).unwrap();
   println!("Hello, {}", user.name().to_str().unwrap());
+
+  // Get the current hostname
+  use libc::{ c_char, sysconf, _SC_HOST_NAME_MAX };
+  use std::ffi::OsString;
+
+  let hostname_max = unsafe { sysconf(_SC_HOST_NAME_MAX) };
+  let mut buffer = vec![0; (hostname_max as usize) + 1];
+  let returncode = unsafe{ libc::gethostname(buffer.as_mut_ptr() as *mut c_char, buffer.len()) };
+
+  if returncode != 0 {
+    println!("Failed to fetch hostname");
+  }
+
+  let end = buffer.iter().position(|&b| b == 0).unwrap_or(buffer.len());
+  buffer.truncate(end);
+  println!("{}", OsString::from_vec(buffer).to_str().unwrap());
 }
